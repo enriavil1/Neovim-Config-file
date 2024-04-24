60bf5d847b57aad0c8c5ff0d42f9ca4b5a17259e
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2024-04-25 00:42:24 +0100

    wow working set up

--- init.lua
+++ init.lua
@@ -1,18 +1,17 @@
-require "user.options"
-require "user.plugins"
-require "user.keymaps"
-require "user.cmp"
-require "user.lsp"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.gitsign"
-require "user.nvimtree"
-require "user.bufferline"
-require "user.toggleterm"
-require "user.alpha"
-require "user.impatient"
-require "user.telescope"
-require "user.lualine"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require "user.colorscheme"
+require("configs.keymaps")
+require("configs.options")
+
+require("lazy").setup("plugins")
