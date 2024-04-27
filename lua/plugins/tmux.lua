a4b59451f7a358771725284c6335980b6409f37c
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2024-04-27 16:03:46 +0100

    updated alpha

---
+++ lua/plugins/tmux.lua
@@ -0,0 +1,10 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  keys = {
+    { "<c-h>",  ":TmuxNavigateLeft<cr>" },
+    { "<c-j>",  ":TmuxNavigateDown<cr>" },
+    { "<c-k>",  ":TmuxNavigateUp<cr>" },
+    { "<c-l>",  ":TmuxNavigateRight<cr>" },
+    { "<c-\\>", ":TmuxNavigatePrevious<cr>" },
+  },
+}
--- lua/plugins/ui/nvim-notify.lua
+++ lua/plugins/ui/nvim-notify.lua
@@ -1,9 +1,17 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
+  keys = {
+    "<leader>dm", ":Noice dismiss<CR>", desc = "Dismiss notifications"
+  },
   opts = {
     cmdline = {
       view = "cmdline"
+    },
+    lsp = {
+      hover = {
+        silent = true
+      }
     }
   },
   dependencies = {
