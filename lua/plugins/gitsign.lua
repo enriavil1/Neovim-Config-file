ee96c3ced6a00aaed0b8260d9fcb55f0ea992486
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2025-05-04 12:38:13 +0100

    update

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -1,29 +1,38 @@
 return {
-  {
-    "tpope/vim-fugitive"
-  },
-  {
-    "lewis6991/gitsigns.nvim",
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
 
-    config = function()
-      require("gitsigns").setup({
-        signs = {
-          add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-          change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-          delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-          topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-          changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        },
-        current_line_blame_opts = {
-          virt_text = true,
-          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-          delay = 1000,
-          ignore_whitespace = false,
-        },
-      })
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				signs_staged = {
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = false,
+				},
+			})
 
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
-    end
-  }
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
+		end,
+	},
 }
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,9 +1,11 @@
-local mason_config = require('plugins.lsp.mason')
-local mason_lspconfig = require('plugins.lsp.mason-lspconfig')
-local nvim_lspconfig = require('plugins.lsp.nvim-lspconfig')
+local mason_config = require("plugins.lsp.mason")
+local mason_lspconfig = require("plugins.lsp.mason-lspconfig")
+local nvim_lspconfig = require("plugins.lsp.nvim-lspconfig")
+local trouble = require("plugins.lsp.trouble")
 
 return {
-  mason_config,
-  mason_lspconfig,
-  nvim_lspconfig
+	mason_config,
+	mason_lspconfig,
+	nvim_lspconfig,
+	trouble,
 }
